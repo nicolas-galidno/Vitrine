5fde8a6788a8834f9724b61bff8147ee5fc795ad
nicolas-galidno <rm99535@estudante.fieb.edu.br>
2026-08-12 17:24:57 +0000

    criando ambiente

---
+++ vitrine/src/test/java/com/itb/tcc/api/vitrine/VitrineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.api.vitrine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitrineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
